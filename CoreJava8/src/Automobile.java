60f2b69c5825aed6b7aa393085bcd33861096299
pracks224 <p.sh.433@gmail.com>
2023-03-03 17:16:40 +0530

    Prefix_sum_hacker_earth

--- CoreJava8/src/Automobile.java
+++
@@ -1,5 +0,0 @@
-public class Automobile {
-    private String drive() {
-        return "Driving vehicle";
-    }
-}
--- CoreJava8/src/Car.java
+++
@@ -1,5 +0,0 @@
-public class Car extends Automobile{
-    protected String drive() {
-        return "Driving car";
-    }
-}
